d665f46503ab5a1e53365984c49532dea9fcc2d7
venky123895 <devathotivenkataramana@gmail.com>
2023-11-24 22:30:42 +0530

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import NavBar from './Components/navBar/NavBar';
 import SimpleSidebar from './Components/sidebar/SideBar';
 import {Routes,Route} from 'react-router-dom'
 import ImagePage from './Components/Pages/ImagePage';
 import { AuthModal } from './Components/Authentication/AuthModal';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
   const[user,setUser]=useState<any>(null)
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,user=>{
+      if(user) setUser(user)
+      else setUser(null)
+    })
+  },[])
+  
   return (
     <div className="App">
-      <NavBar/>
-      <AuthModal/>
-      <Routes>
-        <Route path='/' element={<SimpleSidebar/>}>
-          <Route path='/' element={<ImagePage/>}/>
-          <Route path='/*' element={<ImagePage/>}/>
-        </Route>
-      </Routes>
+      <NavBar user={user}/>
+      {
+        user ? (
+          <Routes>
+            <Route path='/' element={<SimpleSidebar/>}>
+                <Route path='/' element={<ImagePage/>}/>
+                <Route path='/*' element={<ImagePage/>}/>
+            </Route>
+          </Routes>
+        ) : <AuthModal user={user}/>
+      }
+      
+      
     </div>
   );
 }
