62c642c134f15f6eea80b7194081e3ca24b6284f
Venkat-Entropik <devathotivenkataramana@gmail.com>
2024-10-06 10:44:06 +0530

    Revert "migrated CRA to Vite | part 1"
    This reverts commit f640c06c85aeb0a74de2ac9898947a719b15fe87.

--- src/Components/config/firebaseConfig.ts
+++ src/Components/config/firebaseConfig.ts
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
